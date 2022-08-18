0dd6face1a6d6f63e933bb324498a4af0e43cc3a
Kulikovsky <loke0720@gmail.com>
2022-08-18 21:02:03 +0600

    changed design

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 20s linear infinite",
+      },
+      colors: {
+        dark: "#202020",
+        grstart: "#626CBC",
+        grcenter: "#5B68D0",
+        grend: "#936BD3",
+        risestart: "#F6CD6C",
+        risefirst: "#F7AF5F",
+        risesecond: "#F6894F",
+        riseend: "#F74755"
+      },
+    },
+  },
+	plugins: [],
+}
